a629de5901d977a95849f65e08b335452eb73d8e
liyustar <liyustar@gmail.com>
2014-05-13 20:39:17 +0800

    * modify MainApplication.java

--- demo/src/com/xtar/demo/app/MainApplication.java
+++ demo/src/com/xtar/demo/app/MainApplication.java
@@ -1,7 +1,25 @@
 package com.xtar.demo.app;
 
 import android.app.Application;
+import android.content.Context;
 
 public class MainApplication extends Application {
+
+	private static Context mContext = null;	// 全局上下文
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		mContext = getApplicationContext();
+	}
+
+	/**
+	 * 获取全局上下文
+	 * @return mContext
+	 */
+	public Context getContext() {
+		return mContext;
+	}
 
 }
