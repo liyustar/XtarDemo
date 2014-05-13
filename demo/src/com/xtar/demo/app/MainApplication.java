225b39860726c38a38d68c9e35a9bab9316531f3
liyustar <liyustar@gmail.com>
2014-05-13 20:51:10 +0800

    * formatting

--- demo/src/com/xtar/demo/app/MainApplication.java
+++ demo/src/com/xtar/demo/app/MainApplication.java
@@ -5,21 +5,22 @@ import android.content.Context;
 
 public class MainApplication extends Application {
 
-	private static Context mContext = null;	// 全局上下文
+    private static Context mContext = null; // 全局上下文
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-		mContext = getApplicationContext();
-	}
+        mContext = getApplicationContext();
+    }
 
-	/**
-	 * 获取全局上下文
-	 * @return mContext
-	 */
-	public Context getContext() {
-		return mContext;
-	}
+    /**
+     * 获取全局上下文
+     * 
+     * @return mContext
+     */
+    public Context getContext() {
+        return mContext;
+    }
 
 }
