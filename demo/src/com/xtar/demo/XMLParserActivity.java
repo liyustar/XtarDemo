4eaffa5708d357c22f3def3f1c9cae2044b91777
liyustar <liyustar@gmail.com>
2014-05-14 17:51:36 +0800

    + add XMLParserActivity.java

---
+++ demo/src/com/xtar/demo/XMLParserActivity.java
@@ -0,0 +1,32 @@
+package com.xtar.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class XMLParserActivity extends Activity implements View.OnClickListener {
+
+    private Button btnSaxParser = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.xmlparser_activity);
+
+        btnSaxParser = (Button) findViewById(R.id.btn_sax);
+        btnSaxParser.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_sax:
+                break;
+            default:
+                break;
+        }
+    }
+
+}
