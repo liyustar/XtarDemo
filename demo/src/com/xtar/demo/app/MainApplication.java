895dae15b09000650e40f8f9788be173cbfe18da
liyustar <liyustar@gmail.com>
2014-05-25 18:34:56 +0800

    * 漏上库，fragment_activity布局

--- demo/src/com/xtar/demo/app/MainApplication.java
+++ demo/src/com/xtar/demo/app/MainApplication.java
@@ -3,6 +3,12 @@ package com.xtar.demo.app;
 import android.app.Application;
 import android.content.Context;
 
+/**
+ * 通过Application类就可以获得context的方法
+ * 
+ * @author lyx
+ * 
+ */
 public class MainApplication extends Application {
 
     private static Context mContext = null; // 全局上下文
