f96927ecd0bd8a5c48efb68f15e7773ffff48dfe
liyustar <liyustar@gmail.com>
2014-07-13 23:28:06 +0800

    * 增加对daytime服务的耗时测试，测试结果在wiznote上

--- demo/src/com/xtar/demo/DaytimeActivity.java
+++ demo/src/com/xtar/demo/DaytimeActivity.java
@@ -9,11 +9,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class DaytimeActivity extends Activity implements View.OnClickListener {
+    static final String TAG = DaytimeActivity.class.getSimpleName();
 
     private Context mContext = null;
     private Button btn_daytime = null;
@@ -22,17 +24,17 @@ public class DaytimeActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.daytime_activity);
         btn_daytime = (Button) findViewById(R.id.btn_daytime);
-        
+
         mContext = this;
         mHandler = new Handler();
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
+        switch (v.getId()) {
             case R.id.btn_daytime:
                 Toast.makeText(this, "daytime", Toast.LENGTH_LONG).show();
                 btn_daytime.setEnabled(false);
@@ -42,17 +44,19 @@ public class DaytimeActivity extends Activity implements View.OnClickListener {
                 break;
         }
     }
-    
+
     public void doDaytime() {
         new Thread(new DaytimeRunnable()).start();
     }
-    
+
     class DaytimeRunnable implements Runnable {
         @Override
         public void run() {
-            
+
             DaytimeSocket sock = new DaytimeSocket();
             String str = null;
+            long startTime, endTime;
+            startTime = System.currentTimeMillis();
             try {
                 sock.connect();
                 str = sock.recvDaytime();
@@ -60,8 +64,11 @@ public class DaytimeActivity extends Activity implements View.OnClickListener {
                 str = "error";
                 e.printStackTrace();
             }
+            endTime = System.currentTimeMillis();
 
-            final String content = str;
+            final long timeInterval = endTime - startTime;
+            final String content = str + "\ntime: " + timeInterval + "ms";
+            Log.i(TAG, content);
 
             mHandler.post(new Runnable() {
                 @Override
@@ -72,19 +79,20 @@ public class DaytimeActivity extends Activity implements View.OnClickListener {
             });
         }
     }
-    
+
     class DaytimeSocket {
         Socket sock = null;
 
         public void connect() throws Exception {
             sock = new Socket("192.168.0.108", 13);
         }
-        
+
         public String recvDaytime() throws Exception {
             InputStream is = sock.getInputStream();
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
             String str = null;
+            sock.setSoTimeout(30 * 1000);
             str = br.readLine();
             return str;
         }
